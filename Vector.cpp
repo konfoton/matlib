d5562fdbc4fd62424a91403218d296aa6b0354e4
konfoton <konrad.burdach@gmail.com>
2024-06-06 16:38:36 +0200

    yolo

---
+++ Vector.cpp
@@ -0,0 +1,39 @@
+#include "Vector.h"
+
+Vector::Vector(int size, int value){
+    vector = new int[size];
+    for(int i = 0; i < size; i++){
+        vector[i] = value; 
+    }
+}
+Vector::~Vector(){
+    delete vector; 
+}
+void Vector::push_back(int element){
+    size += 1;
+    int* vectornew = new int[size];
+    for(int i = 0; i < size - 1; i++){
+        vectornew[i] = vector[i];
+    }
+    vector[size -1] = element;
+    delete[] vector; 
+    vector = vectornew;
+}
+bool Vector::remove(int element){
+    for(int i = 0; i < size; i++){
+        if(vector[i] == element){
+            size -= 1;
+            int* vectornew = new int[size];
+            for(int j =  0; j < i; j++){
+                vectornew[j] = vector[j];
+            }
+            for(int j = i + 1; j < size + 1; j++){
+                vectornew[i - 1] = vector[j];
+            }
+            delete[] vector;
+            vector = vectornew;
+            return false;
+        }
+    }
+    return false; 
+}
---
+++ Vector.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Vector{
+private:
+    int* vector;
+    int size; 
+public:
+    Vector(int size, int value = 0);
+    ~Vector();
+    void push_back(int element);
+    bool remove(int element);
+    
+}; 
